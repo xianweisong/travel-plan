b2ed1c97d4995704c6b0fdb07ad758dc9ede5d63
nothing <59837400+xianweisong@users.noreply.github.com>
2025-08-31 10:59:14 +0800

    chore: move entry file to project root

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'welcome.html'));
+  res.sendFile(path.join(__dirname, 'welcome.html'));
 });
 
 app.listen(port, '0.0.0.0', () => {
